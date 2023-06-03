9d85a48da99ff21ef60cefcd38b01e7a091fd1ff
romalitv <youlikeyoupass@gmail.com>
2023-06-03 20:35:53 +0200

    Added test file

---
+++ src/test/kotlin/LinkedListTest.kt
@@ -0,0 +1,58 @@
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class LinkedListTest {
+
+    @Test
+    fun length() {
+    }
+
+    @Test
+    fun append() {
+    }
+
+    @Test
+    fun appendList() {
+    }
+
+    @Test
+    fun insert() {
+    }
+
+    @Test
+    fun delete() {
+    }
+
+    @Test
+    fun deleteAll() {
+    }
+
+    @Test
+    fun get() {
+    }
+
+    @Test
+    fun findFirst() {
+    }
+
+    @Test
+    fun findLast() {
+    }
+
+    @Test
+    fun testClone() {
+    }
+
+    @Test
+    fun reverse() {
+    }
+
+    @Test
+    fun clear() {
+    }
+
+    @Test
+    fun extend() {
+    }
+}
